a7e85fd8593a2b77d67ec50e28455882d984e3be
Nguyen Hoang Ngoc Tran <nghngtran68@gmail.com>
2020-05-28 15:42:58 +0700

    Revert "fix camera access"
    This reverts commit bc9756b3508b216d5b8c298e917cbe25d2ed16d8.

--- lib/presentations/UI/page/create_note.dart
+++ lib/presentations/UI/page/create_note.dart
@@ -10,7 +10,6 @@ import 'package:note_app/presentations/UI/custom_widget/custom_text_style.dart';
 import 'package:note_app/presentations/UI/custom_widget/tab_bar_note.dart';
 import 'package:note_app/presentations/UI/page/MoreOptionsSheet.dart';
 import 'package:note_app/presentations/UI/page/base_view.dart';
-import 'package:note_app/presentations/UI/page/camera.dart';
 import 'package:note_app/presentations/UI/page/camera_access.dart';
 import 'package:note_app/presentations/UI/page/create_tag.dart';
 import 'package:note_app/presentations/UI/page/customPaint.dart';
@@ -297,7 +296,8 @@ class NoteItemWidget extends StatelessWidget {
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (BuildContext context) {
-                return CameraScreen();
+                var cameras = CameraDescription();
+                return TakePictureScreen(camera: cameras);
               }));
             },
           ),
