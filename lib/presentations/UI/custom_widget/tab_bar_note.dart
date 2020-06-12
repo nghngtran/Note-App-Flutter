4b6f647987426d642e59569d9583fe1bbdfbb85a
Nguyen Hoang Ngoc Tran <nghngtran68@gmail.com>
2020-06-12 15:35:56 +0700

    Revert "Merge pull request #24 from nghngtran/deleteTag"
    This reverts commit 9597be8ed6bdb472b119b3850ab7c9bb3d6a23e5, reversing
    changes made to f621b5cd12f7ccb07b9743948d246ac0e9e9c4ea.

--- lib/presentations/UI/custom_widget/tab_bar_note.dart
+++ lib/presentations/UI/custom_widget/tab_bar_note.dart
@@ -2,178 +2,132 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/application/constants.dart';
 import 'package:note_app/presentations/UI/custom_widget/custom_type_tag.dart';
-import 'package:note_app/presentations/UI/page/base_view.dart';
 import 'package:note_app/utils/bus/tag_bus.dart';
 import 'package:note_app/utils/dao/tag_dao.dart';
 import 'package:note_app/utils/model/tag.dart';
-import 'package:note_app/view_model/list_tag_view_model.dart';
 import 'package:note_app/view_model/note_view_model.dart';
 import 'package:note_app/presentations/UI/custom_widget/custom_text_style.dart';
-import 'package:autocomplete_textfield/autocomplete_textfield.dart';
-import 'package:provider/provider.dart';
-
-//class CreateTagNote extends StatefulWidget {
-//  final NoteViewModel noteModel;
-//  CreateTagNote(this.noteModel);
-////  CreateTagNoteState creatState() => CreateTagNoteState();
-//
-//  @override
-//  State<StatefulWidget> createState() {
-//    return CreateTagNoteState();
-//  }
-//}
 
 class CreateTagNote extends StatelessWidget {
-  Tag tag = Tag();
+  Tag tag = new Tag();
   final textController = TextEditingController();
   final NoteViewModel noteModel;
-
   CreateTagNote(this.noteModel);
-
-  void dispose() {
-    textController.dispose();
-  }
-
   Widget build(BuildContext context) {
     tag.setColor(Colors.green);
-
-    return ChangeNotifierProvider<TagCreatedModel>(
-        create: (BuildContext context) {
-      return TagCreatedModel();
-    }, child: Consumer<TagCreatedModel>(builder: (context, tagCreatedModel, _) {
-      tagCreatedModel.loadData();
-      List<Tag> tagsCreated = tagCreatedModel.getTagCreated();
-      return Container(
-          width: MediaQuery.of(context).size.width / 100 * 80,
-          height: MediaQuery.of(context).size.height / 100 * 20,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            color: Theme.of(context).backgroundColor,
-          ),
-          padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height / 100 * 2),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text("Create new tag",
-                  style: Theme.of(context).textTheme.title.copyWith(
-                      fontWeight: Font.SemiBold,
-                      color: Theme.of(context).iconTheme.color)),
-              SizedBox(height: MediaQuery.of(context).size.height / 100 * 2),
-              Expanded(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                    Wrap(children: <Widget>[DropDownButtonNote(tag)]),
-                    SizedBox(width: MediaQuery.of(context).size.width / 100),
-                    Expanded(
+    return Container(
+        width: MediaQuery.of(context).size.width / 100 * 80,
+        height: MediaQuery.of(context).size.height / 100 * 20,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: Theme.of(context).backgroundColor,
+        ),
+        padding:
+            EdgeInsets.only(top: MediaQuery.of(context).size.height / 100 * 2),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text("Create new tag",
+                style: Theme.of(context).textTheme.title.copyWith(
+                    fontWeight: Font.SemiBold,
+                    color: Theme.of(context).iconTheme.color)),
+            SizedBox(height: MediaQuery.of(context).size.height / 100 * 2),
+            Expanded(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                  Wrap(children: <Widget>[DropDownButtonNote(tag)]),
+                  SizedBox(width: MediaQuery.of(context).size.width / 100),
+                  Expanded(
                       flex: 7,
-                      child: AutoCompleteTextField<Tag>(
-                          controller: textController,
-                          style: TextStyle(color: Colors.black, fontSize: 16.0),
-                          decoration: InputDecoration(
-                              suffixIcon: Container(
-                                width: 85.0,
-                                height: 60.0,
-                              ),
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
-                              filled: true,
-                              hintStyle: TextStyle(color: Colors.black)),
-                          itemSubmitted: (Tag item) {
-                            textController.text = item.title;
-                          },
-                          clearOnSubmit: false,
-                          key: null,
-                          suggestions: tagsCreated,
-                          itemBuilder: (context, item) {
-                            return Container(
-                                height: MediaQuery.of(context).size.height /
-                                    100 *
-                                    6,
-                                padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
-                                child: Text(item.title,
-                                    style: TextStyle(
-                                        color:
-                                            Theme.of(context).iconTheme.color,
-                                        fontSize: 16)));
-                          },
-                          itemSorter: (a, b) {
-                            return a.title.compareTo(b.title);
-                          },
-                          itemFilter: (item, query) {
-                            return textController.text
-                                .toLowerCase()
-                                .startsWith(query.toLowerCase());
-                          }),
-                    ),
-                    SizedBox(
-                        width: MediaQuery.of(context).size.width / 100 * 2),
-                  ])),
-              Expanded(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
+                      child: TextField(
+                        controller: textController,
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Theme.of(context).iconTheme.color),
+                        decoration: InputDecoration(
+                            alignLabelWithHint: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5),
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).iconTheme.color,
+                                    width: 1)),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5),
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).iconTheme.color,
+                                    width: 1)),
+                            hintText: "Enter tag's name",
+                            contentPadding: EdgeInsets.fromLTRB(5, 15, 0, 15),
+                            hintStyle: TextStyle(
+                                color: Theme.of(context).iconTheme.color,
+                                fontSize: 13)),
+                      )),
+                  SizedBox(width: MediaQuery.of(context).size.width / 100 * 2),
+                ])),
+            Expanded(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
 //                SizedBox(width: MediaQuery.of(context).size.width / 100 * 10),
-                    Expanded(
-                        flex: 1,
-                        child: FlatButton(
-                          color: Colors.transparent,
-                          textColor: Theme.of(context).iconTheme.color,
-                          child: Text("Cancel",
-                              style: TextStyle(
+                  Expanded(
+                      flex: 1,
+                      child: FlatButton(
+                        color: Colors.transparent,
+                        textColor: Theme.of(context).iconTheme.color,
+                        child: Text("Cancel",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontFamily: Font.Name,
+                              fontWeight: Font.Regular,
+                            )),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(5),
+                            side: BorderSide(
+                                color: Colors.transparent, width: 0.5)),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: FlatButton(
+                        color: Colors.transparent,
+                        textColor: Theme.of(context).iconTheme.color,
+                        child: Text("Save",
+                            style: TextStyle(
                                 fontSize: 15,
                                 fontFamily: Font.Name,
                                 fontWeight: Font.Regular,
-                              )),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          shape: RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(5),
-                              side: BorderSide(
-                                  color: Colors.transparent, width: 0.5)),
-                        )),
-                    Expanded(
-                        flex: 1,
-                        child: FlatButton(
-                          color: Colors.transparent,
-                          textColor: Theme.of(context).iconTheme.color,
-                          child: Text("Save",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontFamily: Font.Name,
-                                  fontWeight: Font.Regular,
-                                  color: Colors.blue)),
-                          onPressed: () {
+                                color: Colors.blue)),
+                        onPressed: () {
 //                          Provider.of<Tag>(context, listen: false)
 //                              .setTitle(textController.text);
-                            tag.setTitle(textController.text);
+                          tag.setTitle(textController.text);
 //                          Provider.of<Tag>(context, listen: false).setColor()
 //                              .setTitle(textController.text);
-                            (TagBUS()).addTag(tag);
-                            print(tag);
-                            var _listTags = (TagBUS()).getTags();
-                            List<Tag> listTags = List<Tag>();
-                            _listTags.then((list) =>
-                                list.forEach((tag) => listTags.add(tag)));
-                            listTags.forEach((listT) => listT.toString());
+                          (TagBUS()).addTag(tag);
+                          print(tag);
+                          var _listTags = (TagBUS()).getTags();
+                          List<Tag> listTags = List<Tag>();
+                          _listTags.then((list) =>
+                              list.forEach((tag) => listTags.add(tag)));
+                          listTags.forEach((listT) => listT.toString());
 //                          Provider.of<TagCreated>(context, listen: true)
 //                              .addTag(tag);
 
-                            noteModel.addTag(tag);
-                            Navigator.of(context).pop();
-                          },
-                          shape: RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(5),
-                              side: BorderSide(
-                                  color: Colors.transparent, width: 0.5)),
-                        ))
-                  ])),
-            ],
-          ));
-    }));
-//    });
+                          noteModel.addTag(tag);
+                          Navigator.of(context).pop();
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(5),
+                            side: BorderSide(
+                                color: Colors.transparent, width: 0.5)),
+                      ))
+                ])),
+          ],
+        ));
   }
 }
 
@@ -289,6 +243,7 @@ class CustomTagNote extends StatelessWidget {
               margin: EdgeInsets.only(
                   left: 4 * MediaQuery.of(context).size.width / 100,
                   top: MediaQuery.of(context).size.height / 100),
+//            width: MediaQuery.of(context).size.width / 100 * 25,
               height: MediaQuery.of(context).size.height / 100 * 5,
               padding: EdgeInsets.fromLTRB(
                   MediaQuery.of(context).size.width / 100 * 2,
